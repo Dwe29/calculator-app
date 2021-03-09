44e7315002602b1433fa50b8fcdf9293c9e7f79f
Dwi <wahyu5161.w5@gmail.com>
2021-03-09 13:10:50 +0800

    menambahkan function tambah

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace Calculator
 {
     class Program
     {
+        static int tambah(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
 
